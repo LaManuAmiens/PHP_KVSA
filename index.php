70cb9b99a9abff555e879e14b891d8b39f7691d5
Valentin <valentin.scellier@gmail.com>
2018-12-14 14:09:09 +0100

    rad

--- index.php
+++ index.php
@@ -1,62 +1,46 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
+<?php
+$xml = simplexml_load_file("source.xml") or die("Error: Cannot create object");
+?>
+<!doctype html>
+<html lang="fr">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="assets/css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <title>ocordo</title>
+  <title>OCORDO KVSA</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Accueil</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?id=2.html">Qui sommes-nous ?</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?id=3.html">Commentaires client</a>
-        </li>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php?id=4.html">Contacts</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <nav class="navbar navbar-expand-lg">
+  <a class="nav-item nav-link" href="#">OCORDO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+        <?php
+//Grace a foreach je parcours le tableau $xml vers <page> = je recupere son contenu
+        foreach ($xml->page as $pageContent) {
+            ?>
+<!--J'affiche <menu> de chaque page dans ma Navbar-->            
+<a class="nav-item nav-link" href="?id=<?= $pageContent->attributes() ?>.html"><?= $pageContent->menu ?></a>
+            <?php
+        }
+      ?>
+  </div>
   </div>
 </nav>
-<?php
-if($_GET['id'] == '2.html'){
-  $xml1 = simplexml_load_file('source.xml') or die('Erreur : impossible de créer l\'objet');
-  echo $xml1->page[1]->menu;
-  echo $xml1->page[1]->title;
-  echo $xml1->page[1]->content;
-}
-else if($_GET['id'] == '3.html'){
-  $xml1 = simplexml_load_file('source.xml') or die('Erreur : impossible de créer l\'objet');
-  echo $xml1->page[2]->menu;
-  echo $xml1->page[2]->title;
-  echo $xml1->page[2]->content;
-}else if($_GET['id'] == '4.html'){
-  $xml1 = simplexml_load_file('source.xml') or die('Erreur : impossible de créer l\'objet');
-  echo $xml1->page[3]->menu;
-  echo $xml1->page[3]->title;
-  echo $xml1->page[3]->content;
-}else{
-  $xml1 = simplexml_load_file('source.xml') or die('Erreur : impossible de créer l\'objet');
-  echo $xml1->page[0]->menu;
-  echo $xml1->page[0]->title;
-  echo $xml1->page[0]->content;
-}
-?>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!--Grace a foreach je parcours  le tableau $xml vers l'index 0 (= 1 dans le tableau) vers <content> = je recupere son contenu
+$xml-> page-> content EST LA DIRECTION QUE MA BOUCLE DOIT SUIVRE
+Je fais apparaitre sur ma page index.php $value (contenu de <content>)-->
+    <?php echo $xml->page[intval($_GET['id'])-1]->content; ?>
+
+
+    
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
