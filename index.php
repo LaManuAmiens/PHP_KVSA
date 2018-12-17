cf6ad2981019e60200532949ed6b959f85508f40
CAQUINEAU Aline <caquineaualine@live.fr>
2018-12-17 14:34:55 +0100

    commenté

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!--simplexml_load_file — Convertit un fichier XML en objet-->
 <?php
 $xml = simplexml_load_file("source.xml") or die("Error: Cannot create object");
+//La condition if me permet de vérifier qu'il y a la clé 'id' dans le tableau $_GET, 
 if (isset($_GET['id'])){
+//j'initialise la variabble $page qui récupère l'entier de la valeur 'id' , je précise -1 car un tableau commence a Zero
     $page = intval($_GET['id'])-1;
 }else{
+//si je n'arrive pas à récupérer les valeurs d'id j'affiche la page accueil (0)
     $page = 0;
 }
 ?>
@@ -15,7 +18,7 @@ if (isset($_GET['id'])){
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="assets/css/style.css" rel="stylesheet">
-<!--    Dans ce titre, j'echo le titre se trouvant: : sur la page xml -> dans la balise page , correspondant aux  données de (id="x") se trouvant dans sa balise title-->
+<!--Dans ce titre, j'affiche le titre se trouvant: : sur la page xml -> dans la balise page , correspondant aux  données de (id="x") se trouvant dans sa balise title-->
   <title><?= $xml->page[$page]->title; ?></title>
 </head>
 <body>
@@ -36,8 +39,7 @@ if (isset($_GET['id'])){
       ?>
   </div>
 </nav>
-    
-<!--j'echo le contenu se trouvant: : sur la page xml -> dans la balise page , correspondant aux données de (id="x") se trouvant dans sa balise content-->
+<!--j'affiche le contenu se trouvant: : sur la page xml -> dans la balise page, dans le tableau $page , se trouvant dans sa balise content-->
     <?= $xml->page[$page]->content; ?>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -45,7 +47,3 @@ if (isset($_GET['id'])){
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
-
