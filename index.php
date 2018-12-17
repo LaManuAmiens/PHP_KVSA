6d84892196f171943393a9d1710ec7bed0655c7f
Valentin <valentin.scellier@gmail.com>
2018-12-17 09:54:09 +0100

    maj

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ $xml = simplexml_load_file("source.xml") or die("Error: Cannot create object");
 <body>
     <nav class="navbar navbar-expand-lg">
   <a class="nav-item nav-link" id='logoAccueil'>OCORDO</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
         <?php
